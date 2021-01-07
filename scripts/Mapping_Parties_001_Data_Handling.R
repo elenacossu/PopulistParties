819eb8933bc7747a5dd86e60a9cfbba2eb52f31e
Elena Cossu <Elena@MacBook-Pro-di-Elena.local>
2021-01-07 22:32:18 +0100

    stuff

--- scripts/Mapping_Parties_001_Data_Handling.R
+++ scripts/Mapping_Parties_001_Data_Handling.R
@@ -4,7 +4,7 @@ raw_data_2014 <- read.csv('files/CHES_2014_Party_dataset.csv', stringsAsFactors 
 
 populist_input <- read.csv('files/Populist_Parties_Input.csv', sep = ';', stringsAsFactors = FALSE)
 
-country_regions <- read.csv('files/Country_Regions_Input.csv', sep = ';', stringsAsFactors = FALSE)
+country_regions2014 <- read.csv('files/Country_Regions_Input.csv', sep = ';', stringsAsFactors = FALSE)
 
 main_data_2014 <- raw_data_2014 %>% 
   #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -12,13 +12,10 @@ main_data_2014 <- raw_data_2014 %>%
                 eu_position,
                 eu_salience,
                 eu_dissent,
-                eu_benefit,
                 eu_intmark,
                 eu_cohesion,
                 eu_foreign,
                 eu_budgets,
-                eu_ep,
-                eu_turkey,
                 lrgen,
                 lrecon,
                 lrecon_salience,
@@ -38,10 +35,11 @@ main_data_2014 <- raw_data_2014 %>%
                 ethnic_minorities,
                 nationalism,
                 antielite_salience,
-                corrupt_salience) %>% 
+                corrupt_salience) %>%
   #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   dplyr::rename(country = cname,                                                                                #Renaming variables for easier understanding
                 party_code = party_id) %>% 
+  dplyr::filter(country != 'tur') %>% 
   #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   dplyr::mutate(pol_side1 = factor(dplyr::case_when(lrgen <= 4 ~ 1,                                             #Creating a factor variable with political sides, based on the 'lrgen' variable
                                                    lrgen > 4 & lrgen < 6 ~ 2, 
@@ -52,7 +50,7 @@ main_data_2014 <- raw_data_2014 %>%
   #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   dplyr::mutate(populist = factor(populist, labels = c('Not Populist', 'Populist'))) %>%                        #Creating a factor variable from the joint populist variable
   #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-  dplyr::left_join(., country_regions, by = 'country') %>%                                                      #Joining the region dataset to the analysis dataset 
+  dplyr::left_join(., country_regions2014, by = 'country') %>%                                                      #Joining the region dataset to the analysis dataset 
   #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   dplyr::mutate(region1 = factor(region1, labels = c('Western Europe', 'Eastern Europe',                        #Creating a factor variable from the 4-way region variable
                                                      'Northern Europe', 'Southern Europe')),
@@ -75,7 +73,7 @@ missing_values_2014 <- main_data_2014 %>%
 missing_values_2014
 
 ###A party's name was NA and R translated it into 'NA' value, inputting the proper character NA
-main_data_2014[which(main_data_2014$party_code == 2406), "party_name"] <- "NA"
+main_data_2014[which(main_data_2014$party_code == 2406), "party"] <- "NA"
 
 
 ###Checking the populist parties and political sides
@@ -90,22 +88,22 @@ main_data_2014 %>%
 #------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------
-# 2019, change accordinf to data_nap_2014
 
 raw_data_2019 <- read.csv('files/CHES_2019_Party_dataset.csv', stringsAsFactors = FALSE)
+
+populist_input <- read.csv('files/Populist_Parties_Input.csv', sep = ';', stringsAsFactors = FALSE)
+
+country_regions2019 <- read.csv('files/Country_Regions_Input2019.csv', sep = ';', stringsAsFactors = FALSE)
 
 main_data_2019 <- raw_data_2019 %>% 
   dplyr::select(country, party, party_id, lrgen,                                                             #Selecting the variables, which are needed for the analysis
                 eu_position,
                 eu_salience,
                 eu_dissent,
-                eu_benefit,
                 eu_intmark,
                 eu_cohesion,
                 eu_foreign,
                 eu_budgets,
-                eu_ep,
-                eu_turkey,
                 lrgen,
                 lrecon,
                 lrecon_salience,
@@ -117,7 +115,7 @@ main_data_2019 <- raw_data_2019 %>%
                 econ_interven,
                 civlib_laworder,
                 sociallifestyle,
-                religious_principle,
+                religious_principles,
                 immigrate_policy,
                 multiculturalism,
                 urban_rural,
@@ -125,7 +123,52 @@ main_data_2019 <- raw_data_2019 %>%
                 ethnic_minorities,
                 nationalism,
                 antielite_salience,
-                corrupt_salience)
-  
-  
+                corrupt_salience) %>% 
+  #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+dplyr::rename(country = country,                                                                                #Renaming variables for easier understanding
+              party_code = party_id) %>% 
+  dplyr::filter(country != 'tur' & country != 'ice') %>% 
+  #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+dplyr::mutate(pol_side1 = factor(dplyr::case_when(lrgen <= 4 ~ 1,                                             #Creating a factor variable with political sides, based on the 'lrgen' variable
+                                                  lrgen > 4 & lrgen < 6 ~ 2, 
+                                                  lrgen >= 6 ~ 3), 
+                                 labels = c('Left', 'Center', 'Right'))) %>% 
+  #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+dplyr::left_join(., populist_input, by = 'party_code') %>%                                                    #Joining the populist dataset to the analysis dataset
+  #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+dplyr::mutate(populist = factor(populist, labels = c('Not Populist', 'Populist'))) %>%                        #Creating a factor variable from the joint populist variable
+  #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+dplyr::left_join(., country_regions2019, by = 'country') %>%                                                      #Joining the region dataset to the analysis dataset 
+  #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+dplyr::mutate(region1 = factor(region1, labels = c('Western Europe', 'Eastern Europe',                        #Creating a factor variable from the 4-way region variable
+                                                   'Northern Europe', 'Southern Europe')),
+              #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+              region2 = factor(region2, labels = c('Western Europe', 'Eastern Europe'))) %>%                  #Creating a factor variable from the 2-way region variable
+  #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+dplyr::mutate(pol_side2 = factor(dplyr::case_when(populist == 'Populist' ~ 4,                                 #Creating a 4-way political side variable using the political side and the populist variable
+                                                  populist == 'Not Populist' & pol_side1 == 'Left' ~ 1,
+                                                  populist == 'Not Populist' & pol_side1 == 'Center' ~ 2,
+                                                  populist == 'Not Populist' & pol_side1 == 'Right' ~ 3),
+                                 labels = c('Left', 'Center', 'Right', 'Populist')))
+
+###Checking missing values
+missing_values_2019 <- main_data_2019 %>% 
+  as.data.frame %>% 
+  dplyr::summarise_all(~sum(is.na(.))) %>% 
+  tidyr::pivot_longer(dplyr::everything(), names_to = 'variables', values_to = 'missing_values')
+
+missing_values_2019
+
+###A party's name was NA and R translated it into 'NA' value, inputting the proper character NA
+main_data_2014[which(main_data_2014$party_code == 2406), "party"] <- "NA"
+
+###Checking the populist parties and political sides
+table(main_data_2019$populist)
+table(main_data_2019$pol_side1, main_data_2019$populist)
+table(main_data_2019$pol_side2, main_data_2019$populist)
+
+main_data_2019 %>% 
+  dplyr::filter(populist == 'Populist' & pol_side1 == 'Center') %>% 
+  dplyr::select(country, party)
+
 
--- scripts/anova_tests.R
+++ scripts/anova_tests.R
@@ -3,19 +3,6 @@ library(ggpubr)
 library(rstatix)
 library(purrr)
 
-analysis_data <- main_data_2014 %>% 
-  dplyr::filter(country != 'tur') %>% 
-  dplyr::select(-c(country, party_name, lrgen, eu_benefit)) %>% 
-  dplyr::select(party_code, pol_side1, pol_side2, populist, region1, region2, 
-                eu_position, eu_intmark, eu_cohesion, eu_foreign, eu_budgets, eu_ep, eu_turkey,   #1-7 scale variables first
-                dplyr::everything())                                                              #0-10 scale variables second
-
-
-
-test_data <- analysis_data %>% 
-  dplyr::select(-c(party_code, pol_side1, pol_side2, populist, region2,
-                   eu_position, eu_intmark, eu_cohesion, eu_foreign, eu_budgets, eu_ep, eu_turkey))
-
 # Checking the outliers
 test_data %>% 
   dplyr::group_by(region1) %>% 
